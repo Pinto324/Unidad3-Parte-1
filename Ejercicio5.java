b5bde7cd77d27dae25efa30a7d5150f3dc444c4c
Brandon Pinto <branps18@gmail.com>
2021-06-09 01:55:05 -0600

    Ejercicio 5 java

---
+++ Ejercicio5.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int No = 0;
+        System.out.println("Ingrese el numero");
+        No = sc.nextInt();
+        No++;
+        System.out.println("El numero siguiente es: "+No);
+    }
+}
